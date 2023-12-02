e4d08fd3f6aae783c4a57f17fac2f9267af46249
EirikaDoesntCode <eirikadoesntcode@gmail.com>
2023-12-02 21:35:31 +0000

    Day 2 completed first try, lets goooooooooooooooooooooooooooooooooooooooooooooooo

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
 Console.WriteLine("Hello, World!");
 
 
@@ -11,14 +13,64 @@ int value = 0;
 Bag[] bags = new Bag[InLen];
 for(int i = 0; i < InLen; ++i)
 {
-    string[] splitGames = Inputs[i].Split(':', ';');
+    string[] splitGames = Inputs[i].Split(':', ';', ',');
+    int game = 0;
+    int green = 0;
+    int blue = 0;
+    int red = 0;
     outputStrArray(splitGames);
-    
+    for(int j = 0; j < splitGames.Length; ++j)
+    {
+        if(Regex.IsMatch(splitGames[j], "Game"))
+        {
+            if(game < int.Parse(Regex.Match(splitGames[j], @"\d+").Value)) 
+            {
+                game = int. Parse(Regex.Match(splitGames[j], @"\d+").Value);
+            }
+        }
+        if(Regex.IsMatch(splitGames[j], "red"))
+        {
+            if(red < int.Parse(Regex.Match(splitGames[j], @"\d+").Value)) 
+            {
+                red = int. Parse(Regex.Match(splitGames[j], @"\d+").Value);
+            }        }
+        if(Regex.IsMatch(splitGames[j], "blue"))
+        {
+            if(blue < int.Parse(Regex.Match(splitGames[j], @"\d+").Value)) 
+            {
+                blue = int. Parse(Regex.Match(splitGames[j], @"\d+").Value);
+            }        }
+        if(Regex.IsMatch(splitGames[j], "green"))
+        {
+            if(green < int.Parse(Regex.Match(splitGames[j], @"\d+").Value)) 
+            {
+                green = int. Parse(Regex.Match(splitGames[j], @"\d+").Value);
+            }    
+        }
+    }
+
+    bags[i].GameID = game;
+    bags[i].Green = green;
+    bags[i].Blue = blue;
+    bags[i].Red = red;
+
+    // if(bags[i].Red <= 12 && bags[i].Green <= 13 && bags[i].Blue <= 14)
+    // {
+    //     value+=bags[i].GameID;
+    // }
+    bags[i].Power = red * green * blue;
+    value+=bags[i].Power;
+    /*
+
+    if found red > known red,    known red = found red, etc for other colours
+
+    bags[i].Red = red; etc for other colours
+    */
 
 
 } 
 
-
+Console.WriteLine(value);
 
 
 void outputStrArray(string[] strArrayOfThings)
@@ -38,4 +90,5 @@ struct Bag
     public int Red;
     public int Blue;
     public int Green;
+    public int Power;
 }
